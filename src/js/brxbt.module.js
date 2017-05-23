897b7eb943734200cf5e01159a21cc906b6cccef
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-05-23 09:02:46 -0400

    code cleaning

--- src/js/brxbt.module.js
+++ src/js/brxbt.module.js
@@ -8,18 +8,18 @@
 
   function routerConfig($stateProvider, $urlRouterProvider){
 
-  $urlRouterProvider.when('', '/');
+    $urlRouterProvider.when('', '/');
 
-  $stateProvider
-  .state({
-        name: 'home',
-        url: '/',
-        templateUrl: '/templates/home.template.html',
-      })
-  .state({
-        name: 'about',
-        url: '/about',
-        templateUrl: '/templates/about.template.html',
-      });
-    }
+    $stateProvider
+    .state({
+      name: 'home',
+      url: '/',
+      templateUrl: '/templates/home.template.html',
+    })
+    .state({
+      name: 'about',
+      url: '/about',
+      templateUrl: '/templates/about.template.html',
+    });
+  }
 }());
